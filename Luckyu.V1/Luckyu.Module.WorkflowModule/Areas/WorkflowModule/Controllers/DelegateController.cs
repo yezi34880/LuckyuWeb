0d1da19bff7d53350ca4a9815f03aa3890b7090d
yy <yuyang34880@163.com>
2020-10-15 15:27:23 +0800

    增加任务委托实现类和页面 具体内容还没写

---
+++ Luckyu.V1/Luckyu.Module.WorkflowModule/Areas/WorkflowModule/Controllers/DelegateController.cs
@@ -0,0 +1,21 @@
+﻿using Luckyu.App.Organization;
+using Luckyu.App.Workflow;
+using Luckyu.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luckyu.Module.WorkflowModule.Controllers
+{
+    /// <summary>
+    /// 任务委托  /WorkflowModule/Delegate
+    /// </summary>
+    [Area("WorkflowModule")]
+    public class DelegateController : MvcControllerBase
+    {
+        
+    }
+}
